b9cd91c66d6520b56734795dcf9ca6e12bc92dcc
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-04-04 16:14:26 -0300

    definindo a atividade

--- src/com/arthur/operadores/java/Main.java
+++ src/com/arthur/operadores/java/Main.java
@@ -3,5 +3,28 @@ package com.arthur.operadores.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // int
+        int i1 = 10;
+        int i2 = 20;
+
+        // float
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+
+        // chars
+        char c1 = 'x';
+        char c2 = 'y';
+
+        // strings
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+
+
+        // boolean
+        boolean b1 = true;
+        boolean b2 = false;
+
+
     }
 }
