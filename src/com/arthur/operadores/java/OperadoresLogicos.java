490dd8fd1271f25590696d293907d2904a6bd286
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-04-12 15:32:47 -0300

    tipos de operadores logicos

---
+++ src/com/arthur/operadores/java/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package com.arthur.operadores.java;
+
+public class OperadoresLogicos {
+    public static void main (String[] args) {
+        // Quais os tipos de operadores lógicos?
+        /* && = "e", conjunção
+        * || = "ou", disjunção
+        * ! = "não", negação
+        * ^ = "somente se", disjunção exclusiva*/
+    }
+}
